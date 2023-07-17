32678a640228ca820a41d4d21f07a60042c89bae
MuhummadShohag <muhammadshohagislam.software.e@gmail.com>
2023-07-17 06:57:04 +0300

    [Done] add recent book features on home page

---
+++ src/pages/Home/RecentBooks.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import RecentBookCard from "../../components/shared/Card/RecentBookCard";
+import Spinner from "../../components/shared/Loader/Spinner";
+import SectionTitle from "../../components/shared/SectionTitle/SectionTitle";
+import { useAllBooksQuery } from "../../redux/features/books/bookApi";
+import { IBook } from "../../types/book.type";
+
+const RecentBooks = () => {
+    const { data, isLoading, isError } = useAllBooksQuery(undefined);
+
+    let content;
+
+    if (data?.data?.length > 0) {
+        content = data?.data?.map((d: IBook) => (
+            <RecentBookCard key={d._id} data={d} />
+        ));
+    }
+    if (isLoading && !isError) {
+        content = <Spinner style={"col-span-3 h-52"} />;
+    }
+
+    return (
+        <div className="container mx-auto py-24">
+            <SectionTitle title="Recent Book" />
+            <div className="grid grid-cols-3 gap-6 ">{content}</div>
+        </div>
+    );
+};
+
+export default RecentBooks;
